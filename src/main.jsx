d68f519fcafdb423e64ac74d0dcdb9aa8296b082
onisEg <anasabdo704@gmail.com>
2024-08-16 03:15:38 +0400

    edit

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,6 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-import "./assets/js/jquery-1.12.4.min.js";
-import "./assets/js/jquery.slick.min.js";
-// import "./assets/js/lightgallery.min.js";
-// import "./assets/js/wow.min.js";
-import "./assets/js/main.js";
-
 import App from "./App.jsx";
 import "./index.css";
 
