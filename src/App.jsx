47ea4062144b594eb52b9c7edee7975151bee0d1
onisEg <anasabdo704@gmail.com>
2024-08-22 01:20:03 +0400

    #

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import Layout from "./components/layout/Layout";
+import Loader from "./components/Loader/Loader";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+
   return (
     <>
-      <Layout />
+      {loading ? (
+        <div className={`appie-loader ${loading ? "active" : ""}`}>
+          <Loader />
+        </div>
+      ) : (
+        <div className={`appie-visible ${loading === false ? "active" : ""}`}>
+          <Layout />
+        </div>
+      )}
     </>
   );
 }
